1b4ccd95101005e1e177ec61e773b6f6337ec90a
k-max-xiao <k.max.xiao@gmail.com>
2020-03-06 02:33:13 +0000

    Implemented color_echo function instead of setting variables

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/bash
+# This install file will execute the following tasks:
+# 1. back up the original dotfiles files which will be impacted
+# 2. create soft links of the custom dotfiles in home directory
+# 3. source these soft links in the relevant
+
+# to import the common functions before execute tasks
+source ./basics/.functions
+
+# get the root path to the folder of this file
+export DOTFILES_ROOT=$(realpath `dirname $0`)
+
